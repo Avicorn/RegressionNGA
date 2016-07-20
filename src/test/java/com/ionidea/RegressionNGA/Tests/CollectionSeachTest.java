0998438750ab5c35eb40258eca0ad9716904ec5a
Andrey <Andrey@10.11.1.117>
2016-07-20 09:48:37 -0400

    Two more Collection Search tests

--- src/test/java/com/ionidea/RegressionNGA/Tests/CollectionSeachTest.java
+++ src/test/java/com/ionidea/RegressionNGA/Tests/CollectionSeachTest.java
@@ -37,25 +37,63 @@ public class CollectionSeachTest extends TestNgTestBase {
         Enter Search term in to target field
         Click on Search button
         Verify collection-search-result page opened
-        Verify Search term presents as search filter
+        Verify Search term is present as both search filter and part of the page URL
         Verify number of results is > 0
         
         Note: this test doesn't verify quality of search results
     */
     
-    // Verify search by Artist ONLY on simple data
+    // Verify Artist search by Artist field ONLY, on simple data, letters and digits only.
     @Test
-    public void testCollectionSearchByArtist() {
+    public void testCollectionSearchByArtistName() {
         driver.get(m_baseUrl + collectionSearch.pageURL);
-        
+        //Enter Search term in to target field
         collectionSearch.artistNameTextInput.sendKeys(SearchTerm); 
         collectionSearch.artistSearchButton.click();
-        System.out.println(driver.getCurrentUrl());
-        System.out.println(m_baseUrl + collectionSearchResults.pageURL);
+        //Verify collection-search-result page opened
         Assert.assertTrue(driver.getCurrentUrl().contains(m_baseUrl + collectionSearchResults.pageURL)); 
-
+        //Verify Search term is present as a part of the page URL
+        Assert.assertTrue(driver.getCurrentUrl().contains(SearchTerm)); 
+        //Verify Search term is present as a search filter
         WebElement firstSearchTerm = collectionSearchResults.SearchTerms(driver).get(0);
         Assert.assertEquals(SearchTerm, firstSearchTerm.getText());
+        //Verify number of results is > 0
         Assert.assertTrue(collectionSearchResults.SearchResultsTotal(driver) > 0);
     }
+    
+    // Verify Artist search by Key words in title field ONLY, on simple data, letters and digits only.
+    @Test
+    public void testCollectionSearchByKeyWordsInTitle() {
+        driver.get(m_baseUrl + collectionSearch.pageURL);
+        //Enter Search term in to target field
+        collectionSearch.keyWordsInTitleTextInput.sendKeys(SearchTerm); 
+        collectionSearch.artistSearchButton.click();
+        //Verify collection-search-result page opened
+        Assert.assertTrue(driver.getCurrentUrl().contains(m_baseUrl + collectionSearchResults.pageURL)); 
+        //Verify Search term is present as a part of the page URL
+        Assert.assertTrue(driver.getCurrentUrl().contains(SearchTerm)); 
+        //Verify Search term is present as a search filter
+        WebElement firstSearchTerm = collectionSearchResults.SearchTerms(driver).get(0);
+        Assert.assertEquals(SearchTerm, firstSearchTerm.getText());
+        //Verify number of results is > 0
+        Assert.assertTrue(collectionSearchResults.SearchResultsTotal(driver) > 0);
+    }
+    
+    // Verify Artist search with Images Only Checkbox checked, without key terms.
+    @Test
+    public void testCollectionSearchByImagesOnlyCheckbox() {
+        driver.get(m_baseUrl + collectionSearch.pageURL);
+        //Check checkbox
+        collectionSearch.imagesOnlyCheckbox.click(); 
+        collectionSearch.artistSearchButton.click();
+        //Verify collection-search-result page opened
+        Assert.assertTrue(driver.getCurrentUrl().contains(m_baseUrl + collectionSearchResults.pageURL)); 
+        //Verify Checkbox filter is present as a part of the page URL
+        Assert.assertTrue(driver.getCurrentUrl().contains("Images_online")); 
+        //Verify Checkbox filter is present as a search filter
+        WebElement firstSearchTerm = collectionSearchResults.SearchTerms(driver).get(0);
+        Assert.assertEquals("Images online", firstSearchTerm.getText());
+        //Verify number of results is > 0
+        Assert.assertTrue(collectionSearchResults.SearchResultsTotal(driver) > 0);
+    }    
 }
