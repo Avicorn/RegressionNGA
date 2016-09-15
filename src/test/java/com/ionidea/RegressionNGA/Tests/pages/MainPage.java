9de9b3e225615dd3485e34b03c2aeb41d0cb25af
TestQA <testqa@altair>
2016-09-15 18:52:26 +0300

    The  test for footer ("FooterTest") class has been added

--- src/test/java/com/ionidea/RegressionNGA/Tests/pages/MainPage.java
+++ src/test/java/com/ionidea/RegressionNGA/Tests/pages/MainPage.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.support.How;
 
 public class MainPage extends Page {
     
-    //Main Menu points    
+//Main Menu points    
     @FindBy (xpath = "//div[@id='header-wrap']/div/ul[@id='mainnav']/li/a")
     public List<WebElement> mainMenuOptions;  
 
@@ -54,8 +54,30 @@ public class MainPage extends Page {
     //Shop   
     @FindBy (xpath = "//div/div/ul/li[9]/ul/li/a")
     public List<WebElement> subMenuShop;
-   
 
+    
+// FOOTER
+    @FindBy (xpath = "//div[@id='footer']/ul[@class='footer-nav']/li/a")
+    public List<WebElement> footerMenu;
+    
+    @FindBy (xpath = "//div[@class='quick-info']/a")
+    public WebElement directions;
+       
+    @FindBy (xpath = "//a[contains(@href, 'https://twitter.com/ngadc')]")
+    public WebElement twitterLink;
+    
+    @FindBy (xpath = "//a[contains(@href, 'https://www.facebook.com/nationalgalleryofart')]")
+    public WebElement facebookLink;
+    
+    @FindBy (xpath = "//a[@href='http://instagram.com/ngadc']")
+    public WebElement instagramLink;
+    
+    @FindBy (xpath = "//a[@href='http://pinterest.com/ngadc']")
+    public WebElement pinterestLink;
+    
+    @FindBy (xpath = "//div[@id='footer']/div[2]/span")
+    public WebElement copyRightLabel;
+    
     
     public MainPage(WebDriver driver) {
         super(driver);
