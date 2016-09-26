0d32d8bc3f3b8c97f909c586430a664c8519fff5
TestQA <>
2016-09-26 18:29:36 +0300

    The ProvenanceInformationPage class has been added

---
+++ src/test/java/com/ionidea/RegressionNGA/Tests/pages/ProvenanceInformationPage.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ionidea.RegressionNGA.Tests.pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ *
+ * @author TestQA
+ */
+public class ProvenanceInformationPage extends Page{
+    
+    public ProvenanceInformationPage(WebDriver driver) {
+        super(driver);
+    }
+    
+    
+}
