ddb68d3e9073303227afe14478bbb7cf941f7b98
TestQA <testqa@altair>
2016-09-26 18:32:36 +0300

    ProvenanceInformation class has been added (empty)

---
+++ src/test/java/com/ionidea/RegressionNGA/Tests/ProvenanceInformation.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ionidea.RegressionNGA.Tests;
+
+/**
+ *
+ * @author TestQA
+ */
+public class ProvenanceInformation extends TestNgTestBase{
+    
+    
+}
